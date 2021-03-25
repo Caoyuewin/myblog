934595cc614d1b2f6a4628479061a823ba5aca9a
caoyuewen <caoyuewen@hzyuewan.com>
2021-03-25 13:26:03 +0800

    test

---
+++ src/main/java/com/waibiwaibi/myblog/controller/TestController.java
@@ -0,0 +1,11 @@
+package com.waibiwaibi.myblog.controller;
+
+/**
+ * Copyright©2020 杭州悦玩网络科技有限公司
+ *
+ * @author CaoYuewen
+ * @date {2021/3/25}
+ * @description
+ */
+public class TestController {
+}
